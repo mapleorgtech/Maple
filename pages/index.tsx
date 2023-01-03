a1703fc8481af6a1bf8663175747069babb4f935
Pratyush-Udhani <pratyush.udhani@gmail.com>
2023-01-03 11:12:15 +0530

    refactor

--- pages/index.tsx
+++ pages/index.tsx
@@ -7,14 +7,14 @@ import Link from "next/link";
 const Home: NextPage = () => {
 	return (
 		<>
-				<Head>
-					<title>Login Page</title>
-					<meta name='viewport' content='width=device-width, initial-scale=1' />
-					<link rel='icon' href='/favicon.ico' />
-				</Head>
-				<main>
-					<Login />
-				</main>
+			<Head>
+				<title>Login Page</title>
+				<meta name='viewport' content='width=device-width, initial-scale=1' />
+				<link rel='icon' href='/favicon.ico' />
+			</Head>
+			<main>
+				<Login />
+			</main>
 		</>
 	);
 };
