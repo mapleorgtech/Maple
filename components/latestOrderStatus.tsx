a5cb310f6dbeb4053f8d848581c4555be024003e
Pratyush-Udhani <pratyush.udhani@gmail.com>
2023-01-05 15:40:54 +0530

    made menu component resuable

---
+++ components/latestOrderStatus.tsx
@@ -0,0 +1,15 @@
+import Link from "next/link";
+import styled from "styled-components";
+
+export default function LatestOrderStatus () {
+    return(
+        <Wrapper>
+            <p>ahjksbdkashjbd</p>
+        </Wrapper>
+    )
+}
+
+const Wrapper = styled.div`
+    background: #404040;
+    border-radius: 10px;
+`
