9c9cc621f96191ef4916598325c9fd7cdaa1aeb8
Pratyush-Udhani <pratyush.udhani@gmail.com>
2023-01-06 12:36:04 +0530

    added backdrops

--- pages/distributor/dashboard.tsx
+++ pages/distributor/dashboard.tsx
@@ -3,35 +3,50 @@ import styled from "styled-components";
 import Menu from "../../components/menu";
 import LatestOrderStatus from "../../components/latestOrderStatus";
 
-
 export default function Dashboard() {
-    return(
-        <Container>
-            <Navbar/>
-            <Backdrop>
-                <LatestOrderStatus/>
-            </Backdrop>
-        </Container>
-    )
+	return (
+		<Container>
+			<Navbar />
+			<BackdropOne>
+				<LatestOrderStatus />
+			</BackdropOne>
+			<BackdropTwo>
+				<LatestOrderStatus />
+			</BackdropTwo>
+		</Container>
+	);
 }
 
 const Navbar = styled(Menu)`
-    display: block;
-    left: 0px;
-`
+	display: block;
+	left: 0px;
+	flex-grow: 1;
+`;
 
-const Backdrop = styled.div`
-    display: block;
-    background: rgba(51, 63, 58, 0.5);
-    backdrop-filter: blur(2.5px);
-    border-radius: 15px;
-`
+const BackdropOne = styled.div`
+	display: block;
+	background: rgba(51, 63, 58, 0.5);
+	backdrop-filter: blur(2.5px);
+	border-radius: 15px;
+    margin: 0px 20px 0px 20px;
+	flex-grow: 1;
+`;
+
+const BackdropTwo = styled.div`
+	display: block;
+	background: rgba(51, 63, 58, 0.5);
+	backdrop-filter: blur(2.5px);
+    margin: 0px 20px 0px 0px;
+	border-radius: 15px;
+	flex-grow: 8;
+`;
 
 const Container = styled.div`
-    width: fit-content;
-    height: max-content;
-    display: flex;
-    padding-top: 150px;
-    flex-direction: row;
-    justify-content: space-around;
-`
+	width: 100%;
+	height: max-content;
+	display: flex;
+	padding-top: 150px;
+	align-items: stretch;
+	flex-direction: row;
+	justify-content: space-around;
+`;
